598e2ddcc9375654157ddc40612d21905e52a713
jmeirink <84925553+jmeirink@users.noreply.github.com>
2022-03-29 09:30:48 -0500

    I created a React app and used JSX and Javascript to add HTML and a dynamically created date year within the React app.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const myName = "Jesse Meirink";
+const currentYear = /\d{4}/.exec(Date())[0];
+
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+//Copyright CURRENTYEAR. Current year is added dynamically
+ReactDOM.render(
+  <div>
+    <p>Created by {myName}</p>
+    <p>Copyright {currentYear}.</p>
+  </div>,
+  document.getElementById("root")
+);
